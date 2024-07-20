4c91696f6f35397d7201246a4366fa53f82a58e7
Letícia Keuffer <lekeuffer@gmail.com>
2024-07-20 16:55:07 -0300

    setinha

---
+++ Assets/Scripts/Npcs/NPCs.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public abstract class NPCs : MonoBehaviour, IInteractble
+{
+    [SerializeField] private SpriteRenderer _interagirSprite;
+    private const float DISTANCIA = 1f;
+
+    void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame && DaParaInteragir())
+        {
+            Debug.Log("npcccc");
+            Interagir();
+        }
+
+        if (_interagirSprite.gameObject.activeSelf && !DaParaInteragir())
+            _interagirSprite.gameObject.SetActive(false);
+        else if (!_interagirSprite.gameObject.activeSelf && DaParaInteragir())
+            _interagirSprite.gameObject.SetActive(true);
+
+    }
+    public abstract void Interagir();
+
+    bool DaParaInteragir()
+    {
+        Debug.Log(Vector2.Distance(JogadorController.Instance.transform.position, transform.position));
+        if (Vector2.Distance(JogadorController.Instance.transform.position, transform.position) < DISTANCIA)
+            return true;
+        else return false;
+    }
+}
