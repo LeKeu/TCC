9caf07c524e2a4629701c98b7cbe10d71c88a542
Letícia Keuffer <lekeuffer@gmail.com>
2024-08-08 16:30:31 -0300

    escudo cooldown

--- Assets/Scripts/Jogador/Habilidades.cs
+++ Assets/Scripts/Jogador/Habilidades.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,6 +16,8 @@ public class Habilidades : MonoBehaviour
     #region ESCUDO
     [SerializeField] GameObject EscudoObj;
     [SerializeField] private float escudoTempo = 5f;
+    [SerializeField] float escudoCoolDown = 5f;
+    bool escudoAtivo;
     #endregion
 
     JogadorVida jogadorVida;
@@ -35,13 +38,14 @@ public class Habilidades : MonoBehaviour
 
     private void Update()
     {
+        
         if (Keyboard.current.rKey.wasPressedThisFrame)
             Escudo();
     }
 
     public void Escudo()
     {
-        if (escudo)
+        if (escudo && !escudoAtivo)
         {
             StartCoroutine(EscudoRoutine());
         }
@@ -50,12 +54,19 @@ public class Habilidades : MonoBehaviour
     IEnumerator EscudoRoutine()
     {
         Debug.Log("escudo");
+        escudoAtivo = true;
         GameObject escudoOjbInst = Instantiate(EscudoObj, gameObject.transform);
         escudoOjbInst.transform.parent = gameObject.transform;
         jogadorVida.podeLevarDano = false;
+
         yield return new WaitForSeconds(escudoTempo);
+
         Destroy(escudoOjbInst);
         jogadorVida.podeLevarDano = true;
 
+        yield return new WaitForSeconds(escudoCoolDown);
+
+        escudoAtivo = false;
+        Debug.Log("Escudo Liberado");
     }
 }
