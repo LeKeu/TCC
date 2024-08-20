a539a97981451ea4a19a05fe44d30f5083474acf
Letícia Keuffer <lekeuffer@gmail.com>
2024-08-19 22:16:48 -0300

    Dialogos diferentes npc

--- Assets/Scripts/Npcs/Pedrinho.cs
+++ Assets/Scripts/Npcs/Pedrinho.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class Pedrinho : NPCs, ITalkable
 {
-    [SerializeField] private List<DialogoTexto> dtTutorial;
-
+    [SerializeField] private List<DialogoTexto> dt;
     [SerializeField] private DialogoController dialogoController;
 
     int indexAtual = 0;
 
+    string nome = "Pedrinho";
+    [SerializeField] private Sprite perfil;
+
     public override void Interagir()
     {
-        Falar(dtTutorial[indexAtual]);
+        Falar(dt[indexAtual]);
 
         if (JogadorController.Instance.podeMover) // se o jogador pode se mover, no caso só ocorre quando a conversa acabou
         {
             indexAtual++;
-            if (indexAtual == dtTutorial.Count) { indexAtual = 0; }
+            if (indexAtual == dt.Count) { indexAtual = 0; }
         }
     }
     public void Falar(DialogoTexto dialogoTexto)
     {
+        dialogoTexto.nome = nome;
+        dialogoTexto.perfilNPC = perfil;
         dialogoController.DisplayProximoParagrafo(dialogoTexto);
     }
 }
--- Assets/Scripts/Npcs/VelhoDoidoBR.cs
+++ Assets/Scripts/Npcs/VelhoDoidoBR.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class VelhoDoidoBR : NPCs, ITalkable
 {
-    [SerializeField] private DialogoTexto dialogoTexto;
+    [SerializeField] private List<DialogoTexto> dt;
+
     [SerializeField] private DialogoController dialogoController;
+
+    int indexAtual = 0;
+
+    string nome = "Velho Doido";
+    [SerializeField] private Sprite perfil;
 
     public override void Interagir()
     {
-        Falar(dialogoTexto);
+        Falar(dt[indexAtual]);
+
+        if (JogadorController.Instance.podeMover) // se o jogador pode se mover, no caso só ocorre quando a conversa acabou
+        {
+            indexAtual++;
+            if (indexAtual == dt.Count) { indexAtual = 0; }
+        }
     }
     public void Falar(DialogoTexto dialogoTexto)
     {
+        dialogoTexto.nome = nome;
+        dialogoTexto.perfilNPC = perfil;
         dialogoController.DisplayProximoParagrafo(dialogoTexto);
     }
 }
